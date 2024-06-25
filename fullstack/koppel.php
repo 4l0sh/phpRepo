50f08914c19506b15f7e9a4d18a31b93fd461826
Ali <2155399@roc-dev.com>
2024-06-25 09:28:17 +0200

    testfile gemaakt

--- fullstack/koppel.php
+++ fullstack/koppel.php
@@ -1,19 +1,22 @@
 <?php
 require('database.php');
+
+// Query to retrieve bands
 $bandquery = "select band from bands;";
 $bandresult = mysqli_query($conn, $bandquery);
 
 $options = '';
 while($row = mysqli_fetch_assoc($bandresult)) {
-    $options.= '<option>'. $row['band']. '</option>';
+    $options.= '<option value="'. $row['band']. '">'. $row['band']. '</option>';
 }
 
+// Query to retrieve events
 $eventquery = "select naam from events;";
 $eventresult = mysqli_query($conn, $eventquery);
 
 $eventOptions = "";
 while($row = mysqli_fetch_assoc($eventresult)) {
-    $eventOptions .= '<option>'. $row['naam']. '</option>';
+    $eventOptions.= '<option value="'. $row['naam']. '">'. $row['naam']. '</option>';
 }
 ?>
 
@@ -24,16 +27,28 @@ while($row = mysqli_fetch_assoc($eventresult)) {
     <body class="gridLayout">
         <div>
             <form action="koppel.php" method="post">
-            <select>
-            <option value="" disabled selected>Select a band</option>
-            <?php echo $options;?>
-            </select>
-            <select>
-                <option value="" disabled selected>Select an event</option>
-                <?php echo $eventOptions ?>
-            </select>
-            <input type="submit" value="submit">
-        </form>
+                <select name="band">
+                    <option value="" disabled selected>Select a band</option>
+                    <?php echo $options;?>
+                </select>
+                <select name="event">
+                    <option value="" disabled selected>Select an event</option>
+                    <?php echo $eventOptions?>
+                </select>
+                <input type="submit" value="submit">
+            </form>
         </div>
     </body>
 </html>
+
+<?php
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $selectedBand = $_POST['band'];
+    $selectedEvent = $_POST['event'];
+
+    // Insert the selected band and event into the table
+    $insertQuery = "INSERT INTO bands_has_events (band, naam) VALUES ('$selectedBand', '$selectedEvent')";
+    mysqli_query($conn, $insertQuery);
+}
+?>
